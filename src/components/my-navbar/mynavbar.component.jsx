66c3c750ca20afae8ac2242a7b52eba232bebbfa
samlv99 <samlv3112@gmail.com>
2021-03-18 14:45:23 +0700

    commit 1

---
+++ src/components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../../assets/icons/my-logo.png";
+import "./mynavbar.styles.css";
+
+const MyNavbar = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect fixed="top" expand="md"  className="animate-navbar nav-theme justify-content-between">
+        <Navbar.Brand href="#home">
+          <img className="logo" src={Logo} alt="My Information Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MyNavbar;
