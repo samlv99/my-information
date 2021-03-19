06164af9144b20b64b3ed72c74cb5d2dfa2bb7b6
samlv99 <samlv3112@gmail.com>
2021-03-19 14:36:32 +0700

    commit4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Slide from "react-reveal/Slide";
+
 
 // components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
 import MyCarousel from "./components/my-carousels/my-carousel.component";
 import TitleMessage from "./components/title-message/title-message.component";
+import Skills from "./pages/skills/skills.component";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="App" style={{ position: "realative" }}>
       <MyNavbar />
       <MyCarousel />
       <TitleMessage />
+
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+        <hr />
+        <Skills />
+        </Slide>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
